975daa0250d8ba036a6cd80516d315ec9247ba51
Daniel Galán y Martins <git@galan.de>
2014-02-09 20:30:36 +0100

    diff

--- src/main/java/de/galan/flupi/Flupi.java
+++ src/main/java/de/galan/flupi/Flupi.java
@@ -14,21 +14,46 @@ public class Flupi {
 	private static Long defaultTimeout;
 
 
+	/**
+	 * If no timeout is passed via the builder, a default timeout can be set with this method for further requests.
+	 * 
+	 * @param timeout Timeout in milliseconds
+	 */
 	public static void setDefaultTimeout(long timeout) {
 		defaultTimeout = timeout;
 	}
 
 
+	/**
+	 * If no timeout is passed via the builder, a default timeout can be set with this method for further requests.
+	 * 
+	 * @param timeout Timeout in human time.
+	 */
 	public static void setDefaultTimeout(String timeout) {
 		defaultTimeout = HumanTime.dehumanizeTime(timeout);
 	}
 
 
+	/**
+	 * Specifies the resource to be requested.
+	 * 
+	 * @param resource The URL to request, including protocol.
+	 * @return The HttpBuilder
+	 */
 	public static HttpBuilder request(String resource) {
 		return defaults(new FluentHttpClient().request(resource));
 	}
 
 
+	/**
+	 * Specifies the resource to be requested.
+	 * 
+	 * @param protocol Protocol of the resource
+	 * @param host Host of the resource
+	 * @param port Port the host is listening on
+	 * @param path Path for the resource
+	 * @return The HttpBuilder
+	 */
 	public static HttpBuilder request(String protocol, String host, Integer port, String path) {
 		return defaults(new FluentHttpClient().request(protocol, host, port, path));
 	}
